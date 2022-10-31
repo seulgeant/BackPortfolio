7745507b41eb7d87660f6e43bad2bc9bf30a0673
Seulgeant <leo_04_10@hotmail.com>
2022-10-31 20:57:47 -0300

    se modifico para imagenes

--- leg/src/main/java/com/portfoliogerez/leg/Dto/DtoEstudio.java
+++ leg/src/main/java/com/portfoliogerez/leg/Dto/DtoEstudio.java
@@ -1,10 +1,13 @@
 package com.portfoliogerez.leg.Dto;
 
+//import com.portfoliogerez.leg.Entity.ImageModel;
+//import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
 public class DtoEstudio {
 
     private String Nivel;
@@ -14,11 +17,14 @@ public class DtoEstudio {
     private String hasta;
     private String estado;
     private String imgest;
+    private String nameImg;
+  //  private Set<ImageModel> estudioImages;
+
 
     public DtoEstudio() {
     }
 
-    public DtoEstudio(String Nivel, String Institucion, String Titulo, String desde, String hasta, String estado, String imgest) {
+    public DtoEstudio(String Nivel, String Institucion, String Titulo, String desde, String hasta, String estado, String imgest,String nameImg ) {
         this.Nivel = Nivel;
         this.Institucion = Institucion;
         this.Titulo = Titulo;
@@ -26,6 +32,9 @@ public class DtoEstudio {
         this.hasta = hasta;
         this.estado = estado;
         this.imgest = imgest;
+ this.nameImg=nameImg;
+        
+
     }
 
 }
