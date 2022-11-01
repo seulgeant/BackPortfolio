97932343b46e9976819d1348e89b57348fef4326
Seulgeant <leo_04_10@hotmail.com>
2022-11-01 06:36:52 -0300

    corrigiendo server

--- leg/src/main/java/com/portfoliogerez/leg/Security/Controller/AuthController.java
+++ leg/src/main/java/com/portfoliogerez/leg/Security/Controller/AuthController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://leg-portfolio.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
