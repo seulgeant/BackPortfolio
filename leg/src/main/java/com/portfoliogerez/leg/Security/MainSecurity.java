f133564c7f6b50abc57cbdaf73c0d60884c19507
Seulgeant <leo_04_10@hotmail.com>
2022-11-01 07:04:49 -0300

    se corrigio salida al servidor

--- leg/src/main/java/com/portfoliogerez/leg/Security/MainSecurity.java
+++ leg/src/main/java/com/portfoliogerez/leg/Security/MainSecurity.java
@@ -8,12 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -44,7 +42,7 @@ public class MainSecurity {
     }
 
     String[] resources = new String[]{
-        "/css/**", "/assets/**", "/js/**", "/auth/login", "/include/**", "/layer/**"};
+        "/css/**", "/assets/**", "/js/**", "/auth**", "/include/**", "/layer/**"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
