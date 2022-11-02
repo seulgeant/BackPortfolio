1f732f0e1427f894a450fe768f594ac19ea5b8b0
Seulgeant <leo_04_10@hotmail.com>
2022-11-02 19:45:56 -0300

    a

--- leg/src/main/java/com/portfoliogerez/leg/LegApplication.java
+++ leg/src/main/java/com/portfoliogerez/leg/LegApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LegApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LegApplication.class, args);
+		SpringApplication.run(LegApplication.class, args); 
 	}
 
 }
